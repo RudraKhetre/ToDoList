fa98ac4d68a73f463a62025d22de8c55db6a0d3a
mahar <mahar@DESKTOP-Q0NN952>
2025-03-17 16:35:10 +0530

    TodoList

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+  task="";
+  taskList:{id:number,task:string}[]=[];
+
+  addTask(){
+    this.taskList.push({id:this.taskList.length+1,task:this.task})
+    console.log(this.taskList);
+    this.task="";
+  }
+  deletetask(taskId:number){
+  this.taskList=  this.taskList
+      .filter((item)=>item.id!=taskId)
+      .map((item,index)=>({...item,id:index+1}));
+  }
+}
